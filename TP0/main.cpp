d49f4098f15942a78c36ccca80acb61944110f08
npotenza <npotenza.ok@gmail.com>
2021-05-12 08:13:25 -0300

    Agregado Main

---
+++ TP0/main.cpp
@@ -0,0 +1,28 @@
+/*****************************************************************************
+ * TP0 - Algoritmos y Programación II                                        *
+ * Alumnos:                                                                  *
+ *          Kevin                                                            *
+ *          Pablo                                                            *
+ *          Juan                                                             *
+ *          Potenza, Nicolás A.     - #97024                                 *
+ *                                                                           *
+ *****************************************************************************/
+
+/***** Bibliotecas Estándar **************************************************/
+#include <string>
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+
+/***** Bibliotecas Propias ***************************************************/
+#include "cmdline.h"
+#include "bignum.h"
+
+/***** Elementos Globales ****************************************************/
+using namespace std;
+
+/***** Función main **********************************************************/
+int main(int argc, char const *argv[]){
+	/* code */
+	return 0;
+}
