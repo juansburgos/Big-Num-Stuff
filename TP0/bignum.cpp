d7e2829bbd6ffbe46a3caa9ea31c37312130b0b9
pablomokorel <pmokorel@fi.uba.ar>
2021-05-22 14:29:43 -0300

    Funcionan3

--- TP0/bignum.cpp
+++ TP0/bignum.cpp
@@ -293,7 +293,7 @@ istream& operator>>(istream &in, Bignum &b){
 
 	c = in.get();
 	while(is_digit){
-		if((c <= ' ') && (b.size == 0)) {
+		if((c == ' ') && (b.size == 0)) {
 		}
 		else if((c == '-') && (b.size == 0)) {
 			signo = true;
