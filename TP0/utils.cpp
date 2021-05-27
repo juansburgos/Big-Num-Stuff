8b07089313566b530ba0e29788dd1098ea2ab93a
juansburgos <jsburgos@fi.uba.ar>
2021-05-27 20:59:12 -0300

    cambio funcion is_negative

--- TP0/utils.cpp
+++ TP0/utils.cpp
@@ -5,17 +5,12 @@ bool validate_dict(const char& op, const char* dict) {
 		if (dict[i] == op)
 			return true;
 	}
-	
+
 	return false;
 }
 
 bool is_negative(const string &s){
-	if (s[0] == MINUS){
-		return true;
-	}
-	else{
-		return false;
-	}
+	return s[0] == MINUS;
 }
 
 size_t zerocount(const unsigned short *digits, const size_t &size){
@@ -32,7 +27,7 @@ size_t zerocount(const unsigned short *digits, const size_t &size){
 	POSCONDICIONES: Devuelve si son iguales o no.
 */
 bool is_digits_equal(const unsigned short *a, const unsigned short *b, const size_t &size){
-	
+
 	for(size_t i = 0; i < size; i++){
 		if(a[i]!=b[i])
 			return false;
